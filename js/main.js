3b260afac99f6544ccd47f32203324592f6ac4be
oliversteidel <67387391+oliversteidel@users.noreply.github.com>
2020-12-15 22:51:44 +0100

    added modal for gallery
    modal for viewing gallery img;
    updated main.js width fuctions for modal behaviour

--- js/main.js
+++ js/main.js
@@ -1,7 +1,44 @@
 $(document).ready(function () {
-    
+
+    const modal = $('.modal'),
+        modalCloseBtn = $('.modal__btn--close');
+    imgWrapper = $('.modal__img-wrapper');
+
+    const openModal = () => {
+        modal.removeClass('closed');
+    }
+
+    const closeModal = () => {
+        modal.addClass('closed');
+    }
+
+    const getImgSrc = (attr) => {
+        var imgSrc = attr;
+        let isAcross = /acr/;
+        return isAcross.test(imgSrc);
+    }
+
+    const setImg = (src, ratio) => {
+        if (ratio) {
+            imgWrapper.append('<img src="' + src + '" class="acr">');
+        } else {
+            imgWrapper.append('<img src="' + src + '" class="up">');
+        }
+
+    }
+
+    $('.gallery__img').click(function () {
+        var img = $(this).attr("src");
+        setImg(img, getImgSrc(img));        
+        openModal();
+    });
+
+    modalCloseBtn.click(function () {
+        closeModal();
+        imgWrapper.empty();
+    });
 
 
 
-    
+
 });
