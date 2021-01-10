9747c6a9da508be899f49486aee9ece47e16b992
oliversteidel <67387391+oliversteidel@users.noreply.github.com>
2021-01-10 18:11:57 +0100

    update js

--- js/main.js
+++ js/main.js
@@ -3,29 +3,40 @@ $(document).ready(function () {
     //nav visibility animation on mobile
     var navOpen = false;
 
+    //transform Burger Icon to cross
     const burgerOpen = () => {
         $('.burger-menu__line--middle').css('opacity', '0');
         $('.burger-menu__line--top').css('transform', 'translateY(10px) rotate(45deg)');
-        $('.burger-menu__line--bottom').css('transform', 'translateY(-10px) rotate(-45deg');        
+        $('.burger-menu__line--bottom').css('transform', 'translateY(-10px) rotate(-45deg');
     }
 
+    //tranform Burger Icon to default
     const burgerClose = () => {
         $('.burger-menu__line--middle').css('opacity', '1');
         $('.burger-menu__line--top').css('transform', 'rotate(0)');
-        $('.burger-menu__line--bottom').css('transform', 'rotate(0');        
+        $('.burger-menu__line--bottom').css('transform', 'rotate(0');
     }
 
     $('.burger-menu').click(() => {
         if (!navOpen) {
-            burgerOpen();            
-            gsap.to(".nav-container", {duration: 0.75, ease: "expo.out", x: 0});                       
+            burgerOpen();
+            gsap.to(".nav-container", { duration: 1, ease: "expo.out", x: 0 });
             navOpen = true;
-        }else{
-            gsap.to(".nav-container", {duration: 0.75, ease: "expo.out", x: '-100%'});                       
+        } else {
+            gsap.to(".nav-container", { duration: 0.75, ease: "expo.in", x: '-100%' });
             burgerClose();
             navOpen = false;
         }
     });
+
+    //close nav when link is clicked
+    $('.nav__link-wrapper').click(() => {
+        if (navOpen) {
+            gsap.to(".nav-container", { duration: 0.75, ease: "expo.out", x: '-100%' });
+            burgerClose();
+            navOpen = false;
+        }
+    })
 
     //animation of nav__links
     var turn = false;
@@ -40,10 +51,6 @@ $(document).ready(function () {
             turn = false;
         }
     });
-
-    
-
-
 
     //slideranimation
     var turnCounter = 1;
@@ -86,6 +93,33 @@ $(document).ready(function () {
     }, 4000);
 
     // show selected gallery-image in modal
+    const galleryImgArr = [];
+
+    var imgPos;
+    
+
+    
+
+    for(let i = 1; i <= $('.gallery__img').length; i++) {
+        galleryImgArr.push($('.gallery__img' + i).attr("src"));
+    }
+    console.log(galleryImgArr);
+
+    
+    
+
+
+    const nextImg = (num) => {
+        gsap.to(".currentImg", {duration: 0.75, ease: "expo.out", x: -100, opacity: 0});
+        imgWrapper.empty();
+        var img = galleryImgArr[num];
+        setImg(img, getImgRatio(img));
+
+    }
+
+
+
+
     const modal = $('.modal'),
         modalCloseBtn = $('.modal__btn--close');
     imgWrapper = $('.modal__img-wrapper');
@@ -107,20 +141,42 @@ $(document).ready(function () {
 
     const setImg = (src, ratio) => {
         if (ratio) {
-            imgWrapper.append('<img src="' + src + '" class="acr">');
+            imgWrapper.append('<img src="' + src + '" class="acr currentImg">');
         } else {
-            imgWrapper.append('<img src="' + src + '" class="up">');
+            imgWrapper.append('<img src="' + src + '" class="up currentImg">');
         }
     }
 
     $('.gallery__img').click(function () {
+        if($(this).attr("class").length == 26) {
+            imgPos = $(this).attr("class").substr(-1);
+        }else{
+            imgPos = $(this).attr("class").substr(-2);
+        }
+        
+        console.log(imgPos);
+
         var img = $(this).attr("src");
         setImg(img, getImgRatio(img));
         openModal();
     });
 
-    modalCloseBtn.click(function () {
+    $('.modal__btn--next').click(function () {
+        nextImg(imgPos);
+        imgPos++;
+    });
+
+    $('.modal__btn--prev').click(function () {
+        if(imgPos > 1) {
+            nextImg(imgPos-1);
+            imgPos--;
+        }
+    });
+
+    modalCloseBtn.click(() => {
         closeModal();
         imgWrapper.empty();
     });
+
+
 });
